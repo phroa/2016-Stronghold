b1a235d8ecfab057402b754705c1d51f63547954
Jack Stratton <jack@phroa.net>
2016-01-24 11:20:26 -0800

    drivetrain: do not invert the right motors
    With more testing, we determined that the actual cause of the reversed
    motors is faulty wiring. They've been rewired and now work without being
    reversed.

--- src/org/usfirst/frc/team4915/stronghold/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team4915/stronghold/subsystems/Drivetrain.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team4915.stronghold.subsystems;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team4915.stronghold.Motors;
 import org.usfirst.frc.team4915.stronghold.commands.ArcadeDrive;
@@ -15,14 +14,6 @@ public class Drivetrain extends Subsystem {
 
     private final RobotDrive robotDrive = new RobotDrive(Motors.FRONT_LEFT, Motors.REAR_LEFT, Motors.FRONT_RIGHT, Motors.REAR_RIGHT);
     private final Joystick joystick;
-
-    // If you put all the motors in a line, they would all move the same
-    // direction. Half of them -- the two on the right side -- need to be
-    // inverted because of this.
-    {
-        this.robotDrive.setInvertedMotor(MotorType.kFrontRight, true);
-        this.robotDrive.setInvertedMotor(MotorType.kRearRight, true);
-    }
 
     public Drivetrain(Joystick joystick) {
         this.joystick = joystick;
